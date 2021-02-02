0026a4f922673f38b0a24bad7d8b02cc87752873
Jonas Horvei <jonashorvei1@gmail.com>
2021-02-02 03:29:13 +0100

    Changed from public to build in the server server.js file.

--- packages/server/server.js
+++ packages/server/server.js
@@ -12,7 +12,7 @@ app.get('/ping', (request, response) => {
   return response.json('pong')
 })
 
-app.use(express.static('public'))
+app.use(express.static('build'))
 
 const listener = app.listen(process.env.PORT || 3001, function () {
   console.log('Your app is listening on port ' + listener.address().port)
